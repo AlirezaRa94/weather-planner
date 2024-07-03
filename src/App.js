0e214335dae86f3c4fcbbdf478b81f6e5e856635
Alireza Raei <alirezaraei17@gmail.com>
2024-07-03 15:37:32 +1000

    feat: Implement weather fetching feature in App.js
    - Introduce useState hooks for managing state variables: location, displayLocation, weatherData, isLoading, and error.
    - Add handleLocationChange function to update location state based on user input.
    - Implement convertToFlag function to convert country codes into emoji flags using Unicode.
    - Develop fetchWeather async function to:
      1. Trigger loading state.
      2. Fetch geolocation data using the open-meteo geocoding API and update the state with the response.
      3. Handle errors by setting the error state and logging them.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,68 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [location, setLocation] = useState('');
+  const [displayLocation, setDisplayLocation] = useState('');
+  const [weatherData, setWeatherData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  function handleLocationChange(e) {
+    setLocation(e.target.value);
+  }
+
+  function convertToFlag(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map((char) => 127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  }
+
+  async function fetchWeather() {
+    try {
+      setIsLoading(true);
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      if (!geoData.results) throw new Error('Location not found');
+
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+      setDisplayLocation(`${name} ${convertToFlag(country_code)}`);
+
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      setWeatherData(weatherData.daily);
+    } catch (err) {
+      console.err(err);
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Weather Planner</h1>
+      <div>
+        <input
+          value={location}
+          type='text'
+          placeholder='Search from location'
+          onChange={handleLocationChange}
+        />
+      </div>
+      <button onClick={fetchWeather}>Get Weather</button>
+
+      {isLoading && <p>Loading...</p>}
     </div>
   );
 }
